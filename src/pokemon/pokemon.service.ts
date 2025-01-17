4632857ab88dac9bdd90f89c6e1cd1d9bb029af1
Matias Courroux <mcourroux@hotmail.com>
2025-01-17 17:17:25 -0300

    added: pagination

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,6 +5,7 @@ import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PokemonSeed } from 'src/seed/interfaces/pokemon-seed.interface';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -26,8 +27,16 @@ export class PokemonService {
 
     }
 
-    findAll() {
-        return `This action returns all pokemon`;
+    findAll(paginationDto: PaginationDto) {
+        const { limit = 10, offset = 0 } = paginationDto;
+
+        return this.pokemonModel.find()
+            .limit(limit)
+            .skip(offset)
+            .sort({
+                no: 1
+            })
+            .select('-__v');
     }
 
     async findOne(term: string) {
